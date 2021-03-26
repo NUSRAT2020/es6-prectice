97d88b3077da8e6889e59a7b4939c7bed32f2ab4
NUSRAT2020 <ipaazom@gmail.coom>
2021-03-25 21:25:23 -0400

    teplet

---
+++ templet.js
@@ -0,0 +1,13 @@
+// if u want to give space for separate  word 
+const firstName =  "justin" ;
+const lastName = "viber"
+const fullName = firstName + lastName
+const fullName2 = `${firstName} ${lastName} is a good man ${100+200-111/12*5}`
+console.log(fullName2);
+
+// `` thisz sign also separate the line 
+ const multiLine = `asddds
+ sadfrf
+ asd
+ dfghg`
+ console.log(multiLine);
