a102e387f6ce18214e70f0d18808497f63e24c1e
NUSRAT2020 <ipaazom@gmail.coom>
2021-03-25 21:03:28 -0400

    set defult value

---
+++ default.js
@@ -0,0 +1,10 @@
+// function add(num1 ,num2){
+//         return num1 + num2 ;
+// }
+
+ function add(num1 ,num2 = 20 ){
+        return num1 + num2 ;
+        }
+        
+ const result = add (12); 
+ console.log(result);
