fd3ca5bd63af5ad7d0f83379fb7f6ea2a262be5e
NUSRAT2020 <ipaazom@gmail.coom>
2021-03-25 21:51:25 -0400

    aarrow function

---
+++ arrow.js
@@ -0,0 +1,26 @@
+// function doubleIt(num){
+//         return num * 2 ;
+
+// }
+//  const doubleIt = function(num){
+//         return num * 2
+//  }
+const doubleIt = num => num * 2 ; 
+const add = ( x, y) => x + y
+const giveMe = ()=> 30;
+const doMAth = (x , y )=> { 
+        const sum =  x+y
+        const diff = x-y 
+        const result = sum + diff ;
+        return result;
+} ;
+
+const result = doubleIt(10);
+const result2 = add(5 ,6);
+const result3 = giveMe() ;
+const result4 = doMAth(2,4)
+
+console.log( result );
+console.log( result2 );
+console.log( result3 );
+console.log( result4 );
