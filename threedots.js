729b92f71ba3adbce0d555459ebedf13322f8a6c
NUSRAT2020 <ipaazom@gmail.coom>
2021-03-27 17:08:38 -0400

    threedots

---
+++ threedots.js
@@ -0,0 +1,13 @@
+const ages = [ 14, 23, 43, 44 ,54]
+const ages1 = [ 16, 25, 41, 43,52]
+const ages2 = [ 15, 22, 45, 46 ,55];
+console.log(...ages,...ages1,...ages2);
+
+const business = 405;
+const minister = 239;
+const sochib = 543;
+// const maximum = Math.max(business,minister,sochib)
+// console.log(maximum);
+const takapoysha = [234,124,355,65,7]
+const maximum = Math.max(...takapoysha)
+console.log(maximum);
