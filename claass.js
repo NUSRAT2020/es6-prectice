72bfc267b0c9af38eed1dfa6addee074160535ee
NUSRAT2020 <ipaazom@gmail.coom>
2021-03-27 17:44:36 -0400

    class

---
+++ claass.js
@@ -0,0 +1,5 @@
+class Student {
+
+}
+const student = new Student
+const student2 = new Student
---
+++ class.js
@@ -0,0 +1,13 @@
+class Student {
+ constructor (sId, sName){ 
+         this.id = sId
+         this.name = sName
+        this.school = "DDML school"
+ }
+
+}
+// const student = new Student
+// const student2 = new Student
+const student =new Student(12 , "kajol")
+const student2 = new Student(23, "moriom")
+console.log(student , student2);
