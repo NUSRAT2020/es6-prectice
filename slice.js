5f9adc112325efe61f73f61f9dc31e9855992404
NUSRAT2020 <ipaazom@gmail.coom>
2021-03-29 14:10:05 -0400

    slice

---
+++ slice.js
@@ -0,0 +1,8 @@
+const nums =[0,1,2,3,4,5,6,7,8,9]
+const part  = nums.slice(3,6)
+// console.log({part});
+console.log(nums);
+// const remove = nums.splice(5,8)
+// console.log(remove);
+const join = nums.join(" ami")
+console.log(join);
