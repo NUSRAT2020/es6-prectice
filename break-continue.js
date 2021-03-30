4568a2a978a3657abe9086ded1753faaada71786
NUSRAT2020 <ipaazom@gmail.coom>
2021-03-29 20:31:54 -0400

    break-contionue

---
+++ break-continue.js
@@ -0,0 +1,11 @@
+const nums = [0,1,2,-3,4,5,-6,7,-8,9];
+ for (let i = 0; i < nums.length; i++) {
+        //  console.log(nums[i]); 
+         if(nums[i]>8){
+                 break ;
+         }
+         if (nums[i]<0){ 
+                 continue;
+                 
+         } console.log(nums[i]);
+ }
